b903a1167ab9633cbf98e9d6a9ac14a3e5fb343b
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-10 17:45:57 -0300

    Recompilação 10/03/2021 17:05:57

---
+++ test/index.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import Result, { ResultStatus } from '../src/index';
+
+/* Tests */
+describe('index.ts', (): void => {
+    it('constructor', async (): Promise<void> => {
+        const result: Result = new Result(ResultStatus.SUCCESS, {
+            test: 1
+        });
+
+        expect(result).to.exist;
+
+        expect(result.status).to.be.eq('success');
+
+        expect(result.content).to.exist;
+        expect(result.content).have.property('test').which.is.eq(1);
+
+        expect(result.time).to.exist;
+    });
+});
